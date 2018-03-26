fb09d51b9723d61600076311376d2192db5d018e
bpaquette6801 <b.p.paquette@hotmail.com>
2018-03-26 14:57:07 -0400

    connectionbean

---
+++ src/assignment1/LoginListener.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class LoginListener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
